89859a9f3916715ed41cc58c4eb0d94085b0fa0a
youbinpark <you8inpark@gmail.com>
2022-10-09 19:46:31 +0900

    Add: sensorlist로 가는 버튼 추가

--- src/components/buttons/Button.tsx
+++ src/components/buttons/Button.tsx
@@ -15,7 +15,8 @@ const Buttons = ({ background, text, ...props }: ExpandBtnPropsData) => {
 
 const Button = styled.button<{ background: string }>`
   display: flex;
-  margin-top: 20px;
+  height: 36px;
+  margin-top: 10px;
   padding: 10px 15px;
 
   background-color: ${props => props.background};
@@ -25,8 +26,6 @@ const Button = styled.button<{ background: string }>`
   text-align: center;
   font-weight: 600;
   text-decoration: none;
-
-  transition: box-shadow 200ms ease-out;
 
   &:hover {
     cursor: pointer;
