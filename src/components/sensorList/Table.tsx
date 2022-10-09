d2c840c207ac484200bd8d74e0c81cd18130fb3e
Joey <seyoung1st@gmail.com>
2022-10-09 15:10:14 +0900

    Add: 리스트 페이지네이션 구현

--- src/components/sensorList/Table.tsx
+++ src/components/sensorList/Table.tsx
@@ -4,17 +4,18 @@ import styled from 'styled-components';
 import { GlobalFilter } from './filtering/GlobalFilter';
 
 export const Table = ({ columns, data }: any) => {
-  const { getTableProps, getTableBodyProps, headerGroups, rows, prepareRow, state, setGlobalFilter }: any = useTable(
+  const { getTableProps, getTableBodyProps, headerGroups, page, nextPage, previousPage, canPreviousPage, canNextPage, pageOptions, gotoPage, pageCount, setPageSize, prepareRow, state, setGlobalFilter }: any = useTable(
     {
       // @ts-ignore
       columns,
       data,
     },
     useFilters,
-    useGlobalFilter
+    useGlobalFilter,
+    usePagination
   );
 
-  const { globalFilter } = state;
+  const { globalFilter, pageIndex, pageSize } = state;
 
   return (
     <StyledTable>
@@ -33,7 +34,7 @@ export const Table = ({ columns, data }: any) => {
           ))}
         </thead>
         <tbody {...getTableBodyProps()}>
-          {rows.map((row: any) => {
+          {page.map((row: any) => {
             prepareRow(row);
             return (
               <tr {...row.getRowProps()}>
@@ -45,6 +46,45 @@ export const Table = ({ columns, data }: any) => {
           })}
         </tbody>
       </table>
+      <StyledPagination>
+        <span className='gotoPage'>
+          Go to page:{' '}
+          <input
+            type='number'
+            defaultValue={pageIndex + 1}
+            onChange={e => {
+              const pageNumber = e.target.value ? Number(e.target.value) - 1 : 0;
+              gotoPage(pageNumber);
+            }}
+            style={{ width: '50px' }}
+          ></input>
+        </span>
+        <button className='gotoFirstPage' onClick={() => gotoPage(0)} disabled={!canPreviousPage}>
+          {'<<'}
+        </button>
+        <button onClick={() => previousPage()} disabled={!canPreviousPage}>
+          Previous
+        </button>
+        <span>
+          Page{' '}
+          <span className='pageIndex'>
+            {pageIndex + 1} of {pageOptions.length}
+          </span>{' '}
+        </span>
+        <button onClick={() => nextPage()} disabled={!canNextPage}>
+          Next
+        </button>
+        <button onClick={() => gotoPage(pageCount - 1)} disabled={!canNextPage}>
+          {'>>'}
+        </button>
+        <select className='pageSize' value={pageSize} onChange={e => setPageSize(Number(e.target.value))}>
+          {[10, 25, 50].map(pageSize => (
+            <option key={pageSize} value={pageSize}>
+              Show {pageSize}
+            </option>
+          ))}
+        </select>
+      </StyledPagination>
     </StyledTable>
   );
 };
@@ -66,5 +106,22 @@ const StyledTable = styled.section`
     tr:hover {
       background-color: #ddd;
     }
+  }
+`;
+
+const StyledPagination = styled.div`
+  width: 40%;
+  margin: auto;
+  margin-top: 20px;
+  .pageIndex {
+    font-weight: bold;
+  }
+
+  .gotoFirstPage {
+    margin-left: 15px;
+  }
+
+  .pageSize {
+    margin-left: 15px;
   }
 `;
